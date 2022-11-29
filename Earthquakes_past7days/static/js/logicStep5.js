310c49eb3f9d60e26340af5eb1cbcc87dcdea951
Harman Kaur <harman@192.168.1.7>
2022-11-29 17:14:41 +0530

    Adding files

--- Earthquakes_past7days/static/js/logicStep5.js
+++ Earthquakes_past7days/static/js/logicStep5.js
@@ -1,11 +1,8 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
-
-
+ 
 // Retrieve the earthquake GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
-
-
 
 // This function returns the style data for each of the earthquakes we plot on
 // the map. We pass the magnitude of the earthquake into a function
@@ -21,7 +18,6 @@ function styleInfo(feature) {
       weight: 0.5
     };
   }
-
 // This function determines the color of the circle based on the magnitude of the earthquake.
 function getColor(magnitude) {
     if (magnitude > 5) {
@@ -41,8 +37,7 @@ function getColor(magnitude) {
     }
     return "#98ee00";
   }
-
-  // This function determines the radius of the earthquake marker based on its magnitude.
+// This function determines the radius of the earthquake marker based on its magnitude.
 // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
 function getRadius(magnitude) {
     if (magnitude === 0) {
@@ -50,52 +45,9 @@ function getRadius(magnitude) {
     }
     return magnitude * 4;
   }
-  
-// We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
-    });
-    
-// We create the dark view tile layer that will be an option for our map.
-let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
-    });
-    
-// Create a base layer that holds both maps.
-let baseMaps = {
-    "Streets": streets,
-    "Satellite": satelliteStreets,
-};
-// We define an object that contains the overlays.
- // Create the earthquake layer for our map.
-let earthquakes = new L.layerGroup();
-// This overlay will be visible all the time.
-let overlays = {
-    Earthquakes: earthquakes
-  };
-
-// Create the map object with center, zoom level and default layer.
-let map = L.map('mapid', {
-    center: [39.5, -98.5],
-    zoom: 3,
-    layers: [streets]
-})
-  // Then we add a control to the map that will allow the user to change
-// which layers are visible.
-L.control.layers(baseMaps, overlays).addTo(map);
-
-
-
-
 // Creating a GeoJSON layer with the retrieved data.
 L.geoJSON(data,{
-
     // We turn each feature into a circleMarker on the map.
-
     pointToLayer: function(feature, latlng) {
     console.log(data);
     return L.circleMarker(latlng);
@@ -110,12 +62,9 @@ L.geoJSON(data,{
     }).addTo(earthquakes);
     // Add earthquake layer to our map
     earthquakes.addTo(map);
- 
-
-
 // Adding legends.
-  var legend = L.control({position: 'bottomright'});
-  legend.onAdd = function (map) {
+var legend = L.control({position: 'bottomright'});
+legend.onAdd = function (map) {
       var div = L.DomUtil.create('div', 'info legend')
       const magnitudes = [0, 1, 2, 3, 4, 5];
       const colors = [
@@ -126,16 +75,48 @@ L.geoJSON(data,{
         "#ea822c",
         "#ea2c2c"
       ];
-      
       // loop through our density intervals and generate a label with a colored square for each interval
       for (var i = 0; i < magnitudes.length; i++) {
         console.log(colors[i]);
           div.innerHTML +=
-              '<i style="background:' + getColor(magnitudes[i] + 1) + '"></i> ' +
+              '<i style="background:' + colors[i] + '"></i> ' +
               magnitudes[i] + (magnitudes[i + 1] ? '&ndash;' + magnitudes[i + 1] + '<br>' : '+');
       }
       return div;
   };
-  
   legend.addTo(map);
 });
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        accessToken: API_KEY
+    });
+
+// We create the dark view tile layer that will be an option for our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        accessToken: API_KEY
+    });
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+    "Streets": streets,
+    "Satellite": satelliteStreets,
+    };
+// We define an object that contains the overlays.
+// Create the earthquake layer for our map.
+let earthquakes = new L.layerGroup();
+// This overlay will be visible all the time.
+let overlays = {
+    Earthquakes: earthquakes
+  };
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+    center: [39.5, -98.5],
+    zoom: 3,
+    layers: [streets]
+    })
+L.control.layers(baseMaps, overlays).addTo(map);
